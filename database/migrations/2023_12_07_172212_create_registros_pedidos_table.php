6ca1190d1e8777609f86aaaffdb9d4444ea940ce
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2023-12-07 21:01:26 -0600

    Create 2023_12_07_172212_create_registros_pedidos_table.php

---
+++ database/migrations/2023_12_07_172212_create_registros_pedidos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegistrosPedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registros_pedidos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('id_pedido')->unsigned();
+
+            $table->foreign('id_pedido')->references('id')->on('pedidos')->onDelete("cascade");
+
+            $table->string('tipo'); 
+            $table->string('medida');
+            $table->bigInteger('cantidad');  
+            $table->string('nota');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registros_pedidos');
+    }
+}
