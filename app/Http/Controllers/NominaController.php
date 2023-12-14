01fa8d51aeb52c1f8a49f9512cd64394aab66b78
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2023-12-13 21:03:10 -0600

    Update NominaController.php

--- app/Http/Controllers/NominaController.php
+++ app/Http/Controllers/NominaController.php
@@ -10,94 +10,93 @@ use App\Models\RegistrosNomina;
 
 class NominaController extends Controller
 {
- 
+
     public function __construct()
     {
         $this->middleware('auth');
     }
-   
-    
+
     public function index()
     {
-        
+
         $solicitudes =  Nomina::all()->toArray();
         $cantidad = 0;
-       
+
         //dd(gettype($solicitudes)); 
 
-        $results = array_map(function($element)
-        {
+        $results = array_map(function ($element) {
             //dd($element['id']);
-            $registros = RegistrosNomina::where('id_nomina','=',$element['id'])->get();
+            $registros = RegistrosNomina::where('id_nomina', '=', $element['id'])->get();
             $total = 0;
-            for($i = 0; $i < sizeof($registros); $i++){
+            for ($i = 0; $i < sizeof($registros); $i++) {
                 $total += $registros[$i]->total;
             }
-    
+
             $nomina = new \stdClass();
-            $nomina -> id = $element['id'];
-            $nomina -> fecha = $element['fecha'];
-            $nomina -> total = $total;
+            $nomina->id = $element['id'];
+            $nomina->fecha = $element['fecha'];
+            $nomina->total = $total;
             return $nomina;
         }, $solicitudes);
-       
-        
+
+
         $solicitudes = $results;
-        return view('Nomina.list', compact('solicitudes','cantidad')); 
+        return view('Nomina.list', compact('solicitudes', 'cantidad'));
     }
-    public function create(){
+
+    public function create()
+    {
         return view('Nomina.create');
     }
-    public function show($id){
-        $nomina = RegistrosNomina::where('id_nomina','=',$id)->get();
-  
+
+    public function show($id)
+    {
+        $nomina = RegistrosNomina::where('id_nomina', '=', $id)->get();
+
         return view('Nomina.details', compact('nomina'));
     }
-    public function storage(Request $request){ 
+
+    public function storage(Request $request)
+    {
         try {
-            $dt= Carbon::now()->toDateTimeString();
+            $dt = Carbon::now()->toDateTimeString();
             $nomina = new Nomina();
-            $nomina->fecha= $dt;
+            $nomina->fecha = $dt;
             $nomina->save();
-            
-            $arrayNombres=$request->get('Nombre');
-            $arrayBasura=$request->get('Basura');
-            $arrayCamiseta=$request->get('Camiseta');
-            $arrayJumbo=$request->get('Jumbo');
-            $arrayReempacado=$request->get('Reempacado');
-            $arrayEmpacado=$request->get('Empacado');
 
-            for($id = 0; $id < sizeof($arrayNombres); $id++)
-            {
+            $arrayNombres = $request->get('Nombre');
+            $arrayBasura = $request->get('Basura');
+            $arrayCamiseta = $request->get('Camiseta');
+            $arrayJumbo = $request->get('Jumbo');
+            $arrayReempacado = $request->get('Reempacado');
+            $arrayEmpacado = $request->get('Empacado');
 
+            for ($id = 0; $id < sizeof($arrayNombres); $id++) {
                 $registros = new RegistrosNomina();
-                $registros->name=$arrayNombres[$id];
-                $registros->basura=$arrayBasura[$id]; 
-                $registros->camiseta=$arrayCamiseta[$id];
-                $registros->jumbo=$arrayJumbo[$id];
-                $registros->empacado=$arrayEmpacado[$id];
-                $registros->reempacado=$arrayReempacado[$id];
-                $registros->id_nomina= $nomina->id;
+                $registros->name = $arrayNombres[$id];
+                $registros->basura = $arrayBasura[$id];
+                $registros->camiseta = $arrayCamiseta[$id];
+                $registros->jumbo = $arrayJumbo[$id];
+                $registros->empacado = $arrayEmpacado[$id];
+                $registros->reempacado = $arrayReempacado[$id];
+                $registros->id_nomina = $nomina->id;
                 //Calcular totales
-                $totalB=$arrayBasura[$id]*25*0.5;
-                $totalC=$arrayCamiseta[$id]*25*0.3;
-                $totalJumbo=$arrayJumbo[$id]*25*0.3;
-                $totalEmpacado=$arrayEmpacado[$id];
-                $totalRempacado=$arrayReempacado[$id]*25*0.15;
+                $totalB = $arrayBasura[$id] * 25 * 0.5;
+                $totalC = $arrayCamiseta[$id] * 25 * 0.3;
+                $totalJumbo = $arrayJumbo[$id] * 25 * 0.3;
+                $totalEmpacado = $arrayEmpacado[$id];
+                $totalRempacado = $arrayReempacado[$id] * 25 * 0.15;
 
-                $TotalN=$totalB+$totalC+$totalJumbo+$totalEmpacado+$totalRempacado;
+                $TotalN = $totalB + $totalC + $totalJumbo + $totalEmpacado + $totalRempacado;
 
-                $registros->total= $TotalN;
-                $registros->save();  
+                $registros->total = $TotalN;
+                $registros->save();
             }
-            $mensaje="Correctamente creado";
+            $mensaje = "Correctamente creado";
             return back()->with('mensaje', $mensaje);
-            
         } catch (\Exception $e) {
-            $mensaje="error al crear solicitud";
+            $mensaje = "error al crear solicitud";
             return back()->with('mensaje', $mensaje);
         }
-        
-        
     }
 }
