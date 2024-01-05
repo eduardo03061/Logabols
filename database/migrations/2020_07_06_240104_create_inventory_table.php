f90455fbb060f3430a5c7dd6b41e2f3a69411682
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-01-04 18:55:02 -0600

    Create 2020_07_06_240104_create_inventory_table.php

---
+++ database/migrations/2020_07_06_240104_create_inventory_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->bigInteger('bulks');
+            $table->decimal('kg', 8, 2);
+            $table->string('type');
+            $table->bigInteger('id_user')->unsigned();
+            $table->foreign('id_user')->references('id')->on('users')->onDelete("cascade");
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory');
+    }
+}
