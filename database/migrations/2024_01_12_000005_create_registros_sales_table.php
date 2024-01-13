d952421047ecd6f0cc51d7086022b5d5f7b54646
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-01-12 19:36:35 -0600

    Create 2024_01_12_000005_create_registros_sales_table.php

---
+++ database/migrations/2024_01_12_000005_create_registros_sales_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegistrosSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registros_sales', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('id_sales')->unsigned();
+            $table->foreign('id_sales')->references('id')->on('sales')->onDelete("cascade");
+
+            $table->string('name');
+            $table->bigInteger('bulks');
+            $table->decimal('kg', 8, 2);
+
+            $table->bigInteger('id_item')->unsigned();
+            $table->foreign('id_item')->references('id')->on('inventory')->onDelete("cascade");
+
+            $table->decimal('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registros_sales');
+    }
+}
