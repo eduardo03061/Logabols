894bdde8f8f92c5c18ad5e78cfafb3b1eaaac816
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-01-12 19:01:03 -0600

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('Rol');
+            $table->bigInteger('company_id');
             $table->rememberToken();
             $table->timestamps();
         });
