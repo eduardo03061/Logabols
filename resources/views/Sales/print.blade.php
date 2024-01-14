211ec9a97554832c93e9867e97429800c43a2ce7
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-01-13 23:11:12 -0600

    Create print.blade.php

---
+++ resources/views/Sales/print.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.print')
+
+@section('content')
+    <table>
+        <thead>
+        <tr>
+            <th class="cantidad">CANT</th>
+            <th class="producto">PRODUCTO</th>
+            <th class="precio">$$</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($items as $item)
+            <tr>
+                <td class="cantidad">
+                     {{$item->kg}} kg
+                </td>
+                <td class="producto">
+                    {{$item->name}}
+                </td>
+
+                <td class="precio">
+                    {{$item->price}}
+                </td>
+            </tr>
+        @endforeach
+        <tr>
+            <td class="cantidad"></td>
+            <td class="producto">TOTAL</td>
+            <td class="precio">${{$sales->quantity}}</td>
+        </tr>
+
+        </tbody>
+    </table>
+@endsection
