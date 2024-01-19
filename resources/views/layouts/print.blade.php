238e13a6dcae9444756fc6c7498e1c7342bef8d5
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-01-19 09:30:28 -0600

    Update print.blade.php

--- resources/views/layouts/print.blade.php
+++ resources/views/layouts/print.blade.php
@@ -9,14 +9,20 @@
     <img
         src="{{ asset('assets/images/logo-plasticos-del-cerro.jpg')}}"
         alt="Logotipo">
-    <p class="centrado">Plasticos del cerro
-        <br>CerroYork
-        <br>13/01/2024 08:22 a.m.</p>
+    <p class="centrado" id="dateSale"></p>
     @yield('content')
     <p class="centrado">¡GRACIAS POR SU COMPRA!
         <br>F B: @plasticosdelcerromx</p>
     <br><br><br><br>
     <p>2024</p>
 </div>
+
+<script>
+    let f = new Date();
+    f = f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear();
+    document.getElementById("dateSale").innerHTML = `Plasticos del cerro
+        <br>CerroYork
+        <br>${f}`;
+</script>
 </body>
 </html>
