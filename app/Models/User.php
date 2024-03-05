7e464fdcc585a0adcef917132213f75c920aeb1b
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-03-04 19:02:06 -0600

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Roles;
 
 class User extends Authenticatable
 {
@@ -43,4 +44,35 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function roles()
+    {
+      return $this->belongsToMany(Roles::class,'role_user');
+    }
+    public function authorizeRoles($roles){
+        if (is_array($roles)) {
+          return $this->hasAnyRole($roles) || abort(401, 'This action is unauthorized.');
+        }
+        return $this->hasRole($roles) || abort(401, 'This action is unauthorized.');
+      }
+  
+      public function authorizeRolesWithId($roles){
+     
+        if (is_array($roles)) {
+          return $this->hasAnyRoleWithId($roles) || abort(401, 'This action is unauthorized.');
+        }
+        return $this->hasRoleWithId($roles) || abort(401, 'This action is unauthorized.');
+      }
+  
+      /*** Check multiple roles* @param array $roles*/
+      public function hasAnyRoleWithId($roles){
+        
+        return null !== $this->roles()->whereIn('roles.id', $roles)->first();
+      }
+  
+      /*** Check multiple roles* @param array $roles*/
+      public function hasAnyRole($roles){
+        return null !== $this->roles()->whereIn('name', $roles)->first();
+      }
+     
+    
 }
