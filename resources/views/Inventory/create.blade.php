2deccf2ebf6abf8b10884339fb6b3a4e3449a2ee
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-03-04 19:02:28 -0600

    clean code

--- resources/views/Inventory/create.blade.php
+++ resources/views/Inventory/create.blade.php
@@ -15,27 +15,7 @@
             </button>
             <div class="collapse navbar-collapse justify-content-end">
                 <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown"
-                           aria-haspopup="true" aria-expanded="false">
-                            <i class="material-icons">notifications</i>
-                            <span class="notification">1</span>
-                            <p class="d-lg-none d-md-block">
-                                Some Actions
-                            </p>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">*</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" href="javascript:;" id="navbarDropdownProfile" data-toggle="dropdown"
-                           aria-haspopup="true" aria-expanded="false">
-                            <i class="material-icons">person</i>
-                            <p class="d-lg-none d-md-block">
-                                Account
-                            </p>
-                        </a>
+                    <li class="nav-item dropdown">                         
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
                             <a class="dropdown-item" href="#">Profile</a>
                             <a class="dropdown-item" href="#">Settings</a>
--- resources/views/Pedidos/create.blade.php
+++ resources/views/Pedidos/create.blade.php
@@ -15,19 +15,7 @@
             </button>
             <div class="collapse navbar-collapse justify-content-end">
                 <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown"
-                           aria-haspopup="true" aria-expanded="false">
-                            <i class="material-icons">notifications</i>
-                            <span class="notification">1</span>
-                            <p class="d-lg-none d-md-block">
-                                Some Actions
-                            </p>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">*</a>
-                        </div>
-                    </li>
+                   
                     <li class="nav-item dropdown">
                         <a class="nav-link" href="javascript:;" id="navbarDropdownProfile" data-toggle="dropdown"
                            aria-haspopup="true" aria-expanded="false">
