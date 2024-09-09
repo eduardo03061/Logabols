162d356c6389afded114b078da2e9a70c4e6725c
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-09-09 08:47:43 -0600

    Update list.blade.php

--- resources/views/Inventory/list.blade.php
+++ resources/views/Inventory/list.blade.php
@@ -1,124 +1,120 @@
 @extends('layouts.admin')
 
 @section('content')
-<nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top">
-    <div class="container-fluid">
-        <div class="navbar-wrapper">
-            <a class="navbar-brand" href="">Inventario</a>
-        </div>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="navbar-toggler-icon icon-bar"></span>
-            <span class="navbar-toggler-icon icon-bar"></span>
-            <span class="navbar-toggler-icon icon-bar"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end">
-            <form class="navbar-form">
-                <div class="input-group no-border">
-                    <input type="text" id="search" class="form-control search-bar" placeholder="Buscar..."
-                           onkeyup="searchFunction()"/>
-                    <button type="submit" class="btn btn-white btn-round btn-just-icon">
-                        <i class="material-icons">search</i>
-                        <div class="ripple-container"></div>
-                    </button>
-                </div>
-            </form>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="javascript:;">
-                        <i class="material-icons">dashboard</i>
-                        <p class="d-lg-none d-md-block">
-                            Stats
-                        </p>
-                    </a>
-                </li>
+    <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top">
+        <div class="container-fluid">
+            <div class="navbar-wrapper">
+                <a class="navbar-brand" href="">Inventario</a>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="navbar-toggler-icon icon-bar"></span>
+                <span class="navbar-toggler-icon icon-bar"></span>
+                <span class="navbar-toggler-icon icon-bar"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end">
 
-                <li class="nav-item dropdown">
-                    <a class="nav-link" href="javascript:;" id="navbarDropdownProfile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="material-icons">person</i>
-                        <p class="d-lg-none d-md-block">
-                            Account
-                        </p>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
-                        <a class="dropdown-item" href="#">Profile</a>
-                        <a class="dropdown-item" href="#">Settings</a>
-                        <div class="dropdown-divider"></div>
-
-
-                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                            Cerrar Sesión
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link" href="javascript:;" id="navbarDropdownProfile" data-toggle="dropdown"
+                           aria-haspopup="true" aria-expanded="false">
+                            <i class="material-icons">person</i>
+                            <p class="d-lg-none d-md-block">
+                                Account
+                            </p>
                         </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-            </ul>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
+                            <a class="dropdown-item" href="#">Profile</a>
+                            <a class="dropdown-item" href="#">Settings</a>
+                            <div class="dropdown-divider"></div>
+
+
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                Cerrar Sesión
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
 
-<div class="content" id="vueItemsList">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <a href="{{ route('inventory.create') }}" class="btn btn-success float-right">Agregar</a><br><br>
-                <div class="card">
-                    <div class="card-header card-header-success">
-                        <h4 class="card-title ">Inventario</h4>
-                        <p class="card-category">Lista de Articulos</p>
+    <div class="content" id="vueItemsList">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <a href="{{ route('inventory.create') }}" class="btn btn-success float-right">Agregar</a><br><br>
+                    <div class="input-group no-border">
+                        <input type="text" id="search" class="form-control search-bar" placeholder="Buscar..."
+                               onkeyup="searchFunction()"/>
+                        <button type="submit" class="btn btn-white btn-round btn-just-icon">
+                            <i class="material-icons">search</i>
+                            <div class="ripple-container"></div>
+                        </button>
                     </div>
+                    <div class="card">
+                        <div class="card-header card-header-success">
+                            <h4 class="card-title ">Inventario</h4>
+                            <p class="card-category">Lista de Articulos</p>
+                        </div>
 
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table">
-                                <thead class="thead-dark">
-                                <tr>
-                                    <th>Nombre</th>
-                                    <th>Bultos</th>
-                                    <th>KG</th>
-                                    <th>Tipo</th>
-                                    <th>Unidades</th>
-                                    <th>Acciones</th>
-                                </tr>
-                                </thead>
-                                <tbody id="idTR">
-                                <tr v-for="item in filteredItems" :key="item.id">
-                                    <td><a :href="`Inventory/Details/${item.id}`">@{{ item.name }}</a></td>
-                                    <td class="text-secondary">@{{ item.bulks }}</td>
-                                    <td class="text-secondary">@{{ item.kg }}</td>
-                                    <td class="text-secondary">@{{ item.type }}</td>
-                                    <td class="text-secondary">@{{ item.unidades }}</td>
-                                    <td>
-                                        <div class="dropdown">
-                                            <button class="secondary-btn dropdown-toggle" type="button" id="dropdownMenuButton"
-                                                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                <img src="{{ asset('assets/images/engranaje.svg')}}" alt="" width="20px">
-                                            </button>
+                        <div class="card-body">
+                            <div class="table-responsive overflow-x-auto">
+                                <table class="table">
+                                    <thead class="thead-dark">
+                                    <tr>
+                                        <th>Nombre</th>
+                                        <th>Bultos</th>
+                                        <th>KG</th>
+                                        <th>Tipo</th>
+                                        <th>Unidades</th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody id="idTR">
+                                    <tr v-for="item in filteredItems" :key="item.id">
+                                        <td><a :href="`Inventory/Details/${item.id}`">@{{ item.name }}</a></td>
+                                        <td class="text-secondary">@{{ item.bulks }}</td>
+                                        <td class="text-secondary">@{{ item.kg }}</td>
+                                        <td class="text-secondary">@{{ item.type }}</td>
+                                        <td class="text-secondary">@{{ item.unidades }}</td>
+                                        <td>
+                                            <div class="dropdown">
+                                                <button class="secondary-btn dropdown-toggle" type="button"
+                                                        id="dropdownMenuButton"
+                                                        data-toggle="dropdown" aria-haspopup="true"
+                                                        aria-expanded="false">
+                                                    <img src="{{ asset('assets/images/engranaje.svg')}}" alt=""
+                                                         width="20px">
+                                                </button>
 
-                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 
-                                                <a :href="`Inventory/Edit/${item.id}`" class="dropdown-item">Editar Articulo</a>
+                                                    <a :href="`Inventory/Edit/${item.id}`" class="dropdown-item">Editar
+                                                        Articulo</a>
 
-                                                <form :action="`Inventory/Delete/${item.id}`" method="POST">
-                                                    @csrf
-                                                    <button class="dropdown-item" type="submit">Eliminar</button>
-                                                </form>
+                                                    <form :action="`Inventory/Delete/${item.id}`" method="POST">
+                                                        @csrf
+                                                        <button class="dropdown-item" type="submit">Eliminar</button>
+                                                    </form>
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                </tr>
-                                </tbody>
-                            </table>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 @push('scripts')
@@ -135,7 +131,7 @@
             computed: {
                 filteredItems() {
                     console.log('entra')
-                    console.log('items',this.items)
+                    console.log('items', this.items)
                     return this.items.filter(item => {
                         return item.name.toLowerCase().includes(this.searchQuery.toLowerCase());
                     });
