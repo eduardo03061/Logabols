60fd8c43808cce92e5d39b0bf0b31468e64623f5
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2024-10-22 10:35:13 -0600

    Create privacidad.blade.php

---
+++ resources/views/privacidad.blade.php
@@ -0,0 +1,368 @@
+<!doctype html>
+
+<html lang="en">
+
+
+<head>
+
+    <!-- Required meta tags -->
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Logabols | Bolsa Camiseta</title>
+
+    <meta name="Logabols" content="Logabols">
+
+    <!-- description -->
+
+    <meta name="description" content="Bolsa Ecológica">
+
+    <!-- keywords -->
+
+    <meta name="keywords"
+          content="Business , Bootstrap4, Responsive, HTML5, CSS3, Carousel, Website Template, Revolution, UI Development, Bizler">
+
+
+    <!-- Favicon -->
+
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico"/>
+
+    <!-- Aminate CSS -->
+
+    <link rel="stylesheet" href="{{ asset('css/animate.css') }}"/>
+
+    <!-- Flaticon CSS -->
+
+    <link rel="stylesheet" href="{{ asset('css/flaticon.css') }}"/>
+
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}"/>
+
+    <!-- Owl Carousel stylesheet -->
+
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}"/>
+
+    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css') }}"/>
+
+    <!-- Lightbox CSS -->
+
+    <link rel="stylesheet" href="{{ asset('css/lightbox.css') }}"/>
+
+    <!-- Skin Green Css -->
+
+    <link rel="stylesheet" href="{{ asset('css/skin-green.css') }}"/>
+
+    <!-- Header CSS -->
+
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+
+    <!-- Style CSS -->
+
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}"/>
+
+    <!-- Responsive CSS -->
+
+    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}"/>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
+    <!--[if lt IE 9]>
+
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+    <![endif]-->
+
+    <!-- jquery js-->
+
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+
+    <!-- REVOLUTION CSS -->
+
+    <link rel="stylesheet" href="{{ asset('css/settings.css') }}"/>
+
+    <!-- REVOLUTION JS FILES -->
+
+    <script src="{{ asset('js/revolution/jquery.themepunch.tools.min.js') }}"></script>
+
+    <script src="{{ asset('js/revolution/jquery.themepunch.revolution.min.js') }}"></script>
+
+    <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+
+    <script src="{{ asset('js/revolution/revolution.extension.actions.min.js') }}"></script>
+
+    <script src="{{ asset('js/revolution/revolution.extension.layeranimation.min.js') }}"></script>
+
+    <script src="{{ asset('js/revolution/revolution.extension.parallax.min.js') }}"></script>
+
+    <script src="{{ asset('js/revolution/revolution.extension.slideanims.min.js') }}"></script>
+ 
+     <script async data-ad-client="ca-pub-8578870789807334" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script async src="https://fundingchoicesmessages.google.com/i/pub-8578870789807334?ers=1" nonce="YOUR_NONCE_VALUE"></script>
+
+
+
+</head>
+
+<body>
+
+<!-- ====== Preloader ====== -->
+
+<div class="preloader js-preloader flex-center">
+
+    <div class="dots">
+
+        <div class="dot"></div>
+
+        <div class="dot"></div>
+
+        <div class="dot"></div>
+
+    </div>
+
+</div>
+
+
+<div class="wraper">
+
+    <!-- ====== Header Starts ====== -->
+
+    <header class="header">
+
+        <div class="container">
+
+
+            <nav class="navbar navbar-expand-lg">
+
+
+                <!-- Brand -->
+
+                <h2 class="nav-brand">
+
+                    <a class="navbar-brand" href="index.html" title="Bizler" style="color: green;">Logabols</a>
+
+                </h2>
+
+
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content"
+                        aria-controls="nav-content"
+
+                        aria-expanded="false" aria-label="Toggle navigation">
+
+                    <span class="navbar-toggler-icon"></span>
+
+                    <span class="navbar-toggler-icon"></span>
+
+                    <span class="navbar-toggler-icon"></span>
+
+                </button>
+
+                <!-- Links -->
+
+                <div class="main-menu collapse navbar-collapse" id="nav-content">
+
+                    <ul class="navbar-nav">
+
+                        <li class="nav-item dropdown active">
+
+                            <a class="nav-link" href="{{ url('/') }}">INICIO</a>
+
+                        </li>
+
+                        <li class="nav-item dropdown">
+
+                            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="Productos/">PRODUCTOS</a>
+
+                            <ul class="dropdown-menu">
+
+                                <li class="dropdown-item"><a href="#">Bolsa Camiseta</a></li>
+
+                                <li class="dropdown-item"><a href="#">Rollo Camiseta</a></li>
+
+                                <li class="dropdown-item"><a href="#">Rollo Natural</a></li>
+
+                                <li class="dropdown-item"><a href="#">Rollo Color</a></li>
+
+                                <li class="dropdown-item"><a href="#">Rollo Negro</a></li>
+
+                            </ul>
+
+                        </li>
+
+                        <li class="nav-item dropdown">
+
+                            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">CONTACTO</a>
+
+                            <ul class="dropdown-menu">
+
+                                <li class="dropdown-item"><a href="#" title="Services">Ver Directorio Telefonico</a>
+                                </li>
+
+                                <!--<li class="dropdown-item"><a href="portfolio-3-column.html" title="Services">Portfolio 3 Column</a></li>
+
+                                <li class="dropdown-item"><a href="portfolio-detail.html" title="Services">Portfolio Detail</a></li>-->
+
+                            </ul>
+
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="{{ route('login') }}">ACCESO</a>
+
+                        </li>
+
+                </div>
+
+            </nav>
+
+
+        </div>
+
+    </header>
+ 
+    <!-- ====== Blog Posts Start ====== -->
+
+    <section id="blog-post-list">
+
+    <div class="container mt-5">
+        <h1 class="text-center">Política de Privacidad</h1>
+        <p>En Logabols, valoramos la privacidad de nuestros clientes y usuarios. A continuación, detallamos cómo recopilamos, usamos y protegemos su información personal.</p>
+
+        <h2>Información Recopilada</h2>
+        <p>Podemos recopilar información personal como su nombre, dirección de correo electrónico, número de teléfono y otros datos necesarios para ofrecer nuestros productos y servicios. Esta información es recopilada cuando usted se comunica con nosotros, realiza una compra o interactúa con nuestro sitio web.</p>
+
+        <h2>Uso de la Información</h2>
+        <p>La información recopilada será utilizada para los siguientes fines:</p>
+        <ul>
+            <li>Procesar y gestionar sus pedidos.</li>
+            <li>Contactarlo en caso de ser necesario para completar una transacción.</li>
+            <li>Enviar comunicaciones sobre nuestros productos, promociones y ofertas especiales.</li>
+            <li>Mejorar nuestros productos y servicios basados en sus comentarios y sugerencias.</li>
+        </ul>
+
+        <h2>Protección de Datos</h2>
+        <p>En Logabols, nos comprometemos a garantizar la seguridad de su información. Implementamos medidas de seguridad técnicas y organizativas para evitar el acceso no autorizado a sus datos personales.</p>
+
+        <h2>Compartición de Información</h2>
+        <p>No vendemos ni compartimos su información personal con terceros, excepto en los casos en que sea necesario para cumplir con la ley o con las solicitudes legítimas de las autoridades competentes.</p>
+
+        <h2>Derechos del Usuario</h2>
+        <p>Usted tiene derecho a acceder, modificar o eliminar su información personal. Para ejercer estos derechos, por favor, póngase en contacto con nosotros a través de los medios proporcionados en la sección de contacto.</p>
+
+        <h2>Cookies</h2>
+        <p>Nuestro sitio web utiliza cookies para mejorar su experiencia de navegación. Las cookies son pequeños archivos de texto que se almacenan en su dispositivo cuando visita nuestro sitio web. Usted puede desactivar las cookies en la configuración de su navegador, aunque esto podría afectar la funcionalidad del sitio.</p>
+
+        <h2>Contacto</h2>
+        <p>Si tiene alguna pregunta sobre esta política de privacidad o sobre cómo manejamos su información personal, no dude en ponerse en contacto con nosotros:</p>
+        <ul>
+            <li>Correo electrónico: logabols@yahoo.com.mx</li>
+            <li>Teléfono: 348 783 0850</li>
+            <li>Dirección: Oficina en Arandas, Jalisco</li>
+        </ul>
+
+        <p>Esta política de privacidad fue actualizada por última vez en octubre de 2024.</p>
+       <div id="fcConsentRevocation">
+            <button onclick="googleFC.prompt({consent: googleFC, expireCache: true})">Configuración de privacidad y de cookies</button>
+        </div>
+
+        <a href=”javascript:googlefc.callbackQueue.push(googlefc.showRevocationMessage)”>Configuración de privacidad y de cookies</a>.
+    </div>
+
+    </section>
+
+</div>
+<footer>
+
+    <div class="container">
+        <div class="copyright col-lg-12">
+
+            <div class="row">
+
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"> Copyright &copy; 2024
+
+                    <span class="separator-pipesign"></span>
+
+                    All Rights Reserved
+
+                    <span class="separator-pipesign"></span>
+
+                    <a href="">Logabols</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- ====== Footer End ====== -->
+
+<!-- popper js-->
+<script>
+  window.googleFC = window.googleFC || {
+    cmd: []
+  };
+  googleFC.cmd.push(function() {
+    googleFC.prompt({
+      consent: googleFC,
+      expireCache: true
+    });
+  });
+</script>
+<script src="{{ asset('js/popper.min.js') }}"></script>
+
+<!-- bootstrap js-->
+
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+
+<!-- Waypoints js plugin -->
+
+<script src="{{ asset('js/waypoints.min.js') }}"></script>
+
+<!-- Counter-up js plugin -->
+
+<script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
+
+<!-- Preloadinator -->
+
+<script src="{{ asset('js/jquery.preloadinator.min.js') }}"></script>
+
+<script>
+
+    $('.js-preloader').preloadinator({
+
+        minTime: 2000
+
+    });
+
+</script>
+
+<!-- Lightbox-->
+
+<script src="{{ asset('js/lightbox.js') }}"></script>
+
+<!--imageloaded js plugin -->
+
+<script src="{{ asset('js/imagesloaded.js') }}"></script>
+
+<!-- isotope js -->
+
+<script src="{{ asset('js/isotope.min.js') }}"></script>
+
+<!-- Owl Slider js-->
+<script src="{{ asset('js/owl.carousel.js') }}"></script>
+<script src="{{ asset('js/owl.custom.js') }}"></script>
+<!-- parallax js-->
+<script src="{{ asset('js/jquery.parallax-1.1.3.js') }}"></script>
+<script src="{{ asset('js/jquery.localscroll-1.2.7-min.js') }}"></script>
+<!-- Header menu -->
+<script src="{{ asset('js/jquery.smartmenus.min.js') }}"></script>
+<!-- Custome js-->
+<script src="{{ asset('js/custom.js') }}"></script>
+</body>
+</html>
