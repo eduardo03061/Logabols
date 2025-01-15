6850ab74ff4621f2f64903e02d3d78e0eca48a7d
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2025-01-14 20:26:49 -0600

    Update page.blade.php

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -341,7 +341,7 @@
 
             <div class="row">
 
-                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"> Copyright &copy; 2024
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12"> Copyright &copy; 2025
 
                     <span class="separator-pipesign"></span>
 
